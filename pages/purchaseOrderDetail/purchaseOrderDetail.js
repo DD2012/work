132bd0fb57a3ced69ce0a3dde99697fc49b9adbc
diaoxinrui <443135581@qq.com>
2017-06-04 11:23:27 +0800

    2017-6-4

--- pages/purchaseOrderDetail/purchaseOrderDetail.js
+++ pages/purchaseOrderDetail/purchaseOrderDetail.js
@@ -1,149 +1,156 @@
-define(['text!pages/purchaseOrderDetail/purchaseOrderDetail.html', 'css!pages/purchaseOrderDetail/purchaseOrderDetail', 'uui', 'uuigrid', 'bootstrap', 'uuitree'], function (html) {
+define(['text!pages/purchaseOrderDetail/purchaseOrderDetail.html', 'css!pages/purchaseOrderDetail/purchaseOrderDetail', 'uui', 'uuigrid', 'bootstrap', 'uuitree', './wuliu.js'], function (html) {
     var init = function (element) {
 
 
         var viewModel = {
-            totalPages: '',
-            pageSize: 5,
-            currentPage: 1,
-            totalCount: '',
-            policyProductPage: {
-                totalPages: '',
-                pageSize: 5,
-                currentPage: 1,
-                totalCount: ''
-            },
-            event: {
-                func: function () {
-                    debugger;
-                },
-                pageInit: function () {
-                    viewModel.app = u.createApp({
-                        el: element,
-                        model: viewModel
-                    });
-                    viewModel.searchCondition.setSimpleData([{}]);
-                    viewModel.policyProductSearchCondition.setSimpleData([{}]);
-                    // $('#tree2 li').eq(0)[0].click();
-                },
-                del: function (rowId) {
-                    console.log(rowId);
-                },
-                edit: function (rowId) {
-                    console.log(rowId);
-                },
-                policyProductClick: function () {
-                    viewModel.policyProductDialog = u.dialog({
-                        id: 'policyProductDialog',
-                        content: '#policyProductDialog',
-                        hasCloseMenu: true,
-                        width: '90%',
-                        closeFun: function () {
+                    totalPages: '',
+                    pageSize: 5,
+                    currentPage: 1,
+                    totalCount: '',
+                    policyProductPage: {
+                        totalPages: '',
+                        pageSize: 5,
+                        currentPage: 1,
+                        totalCount: ''
+                    },
+                    event: {
+                        func: function (x) {
+
+                            debugger;
+
+
+                        },
+                        pageInit: function () {
+                            viewModel.app = u.createApp({
+                                el: element,
+                                model: viewModel
+                            });
+                            viewModel.searchCondition.setSimpleData([{}]);
                             viewModel.policyProductSearchCondition.setSimpleData([{}]);
-                            // viewModel.policyProductGrid.setSimpleData([]);
+                            // $('#tree2 li').eq(0)[0].click();
+                        },
+                        del: function (rowId) {
+                            console.log(rowId);
+                        },
+                        edit: function (rowId) {
+                            console.log(rowId);
+                        },
+                        policyProductClick: function () {
+                            viewModel.policyProductDialog = u.dialog({
+                                id: 'policyProductDialog',
+                                content: '#policyProductDialog',
+                                hasCloseMenu: true,
+                                width: '90%',
+                                closeFun: function () {
+                                    viewModel.policyProductSearchCondition.setSimpleData([{}]);
+                                    // viewModel.policyProductGrid.setSimpleData([]);
 
+
+                                }
+                            });
+                            setTimeout(function () {
+                                var rightHeight = $('.dialogRight').height();
+                                $('.dialogLeft').height(rightHeight);
+                                // $('#tree2 li a').eq(0)[0].click();
+                            }, 1000)
+                        },
+                        carCheckClick:function () {
 
                         }
-                    });
-                    setTimeout(function () {
-                        var rightHeight = $('.dialogRight').height();
-                        $('.dialogLeft').height(rightHeight);
-                        // $('#tree2 li a').eq(0)[0].click();
-                    },1000)
-                }
-            },
-            searchCondition: new u.DataTable({
-                meta: {
-                    xxx: {}
-                }
-            }),
-            policyProductSearchCondition: new u.DataTable({
-                meta: {
-                    xxx: {}
-                }
-            }),
+                    },
+                    searchCondition: new u.DataTable({
+                        meta: {
+                            xxx: {}
+                        }
+                    }),
+                    policyProductSearchCondition: new u.DataTable({
+                        meta: {
+                            xxx: {}
+                        }
+                    }),
 
-            purchaseOrderDetailGrid: new u.DataTable({
-                meta: {
-                    xxx: {
-                        default: {
-                            value: '--default--'
+                    purchaseOrderDetailGrid: new u.DataTable({
+                        meta: {
+                            xxx: {
+                                default: {
+                                    value: '--default--'
+                                }
+                            },
+                            handelHook: {}
+                        }
+                    }),
+                    policyProductGrid: new u.DataTable({
+                        meta: {
+                            xxx: {}
+                        }
+                    }),
+                    tree: new u.DataTable({
+                        meta: {
+                            'id': {
+                                'value': ""
+                            },
+                            'pid': {
+                                'value': ""
+                            },
+                            'title': {
+                                'value': ""
+                            }
+                        }
+                    }),
+                    treeSetting: {
+                        view: {
+                            showLine: false,
+                            selectedMulti: false,
+                            selectedId: '01'
+                        },
+                        callback: {
+                            onClick: function (e, id, node) {
+                                console.log('tree-checked', node);
+                                dd();
+                            }
                         }
                     },
-                    handelHook: {}
-                }
-            }),
-            policyProductGrid: new u.DataTable({
-                meta: {
-                    xxx: {}
-                }
-            }),
-            tree: new u.DataTable({
-                meta: {
-                    'id': {
-                        'value': ""
+                    handelHookRender: function (obj) {
+                        var rowId = obj.row.value['$_#_@_id'];
+                        var delfun = "data-bind=click:event.del.bind($data," + rowId + ")";
+                        var editfun = "data-bind=click:event.edit.bind($data," + rowId + ")";
+                        obj.element.innerHTML = '<div><i style="cursor: pointer;" class="op uf uf-pencil font-size-18" title="修改"' + editfun + '></i>' + '<i style="cursor: pointer;" class="font-size-18 op icon uf uf-del title="删除" ' + delfun + '></i></div>';
+                        ko.applyBindings(viewModel, obj.element);
                     },
-                    'pid': {
-                        'value': ""
+                    initPagniation: function () {
+                        var el = document.querySelector("#pagination1");
+                        viewModel.pagination1 = new u.pagination({
+                            el: el,
+                            showState: true,
+                            jumppage: true
+                        });
+                        viewModel.pagination1.on('pageChange', function (currentPage) {
+                            viewModel.currentPage = currentPage + 1;
+
+                        });
+                        viewModel.pagination1.on('sizeChange', function (pageSize) {
+                            viewModel.pageSize = pageSize;
+                            viewModel.currentPage = 1;
+                        });
                     },
-                    'title': {
-                        'value': ""
+                    initPolicyProductPageination: function () {
+                        var el = document.querySelector("#policyProductPageination");
+                        viewModel.policyProductPageination = new u.pagination({
+                            el: el,
+                            showState: true,
+                            jumppage: true
+                        });
+                        viewModel.policyProductPageination.on('pageChange', function (currentPage) {
+                            viewModel.policyProductPage.currentPage = currentPage + 1;
+
+                        });
+                        viewModel.policyProductPageination.on('sizeChange', function (pageSize) {
+                            viewModel.policyProductPage.pageSize = pageSize;
+                            viewModel.policyProductPage.currentPage = 1;
+                        });
                     }
                 }
-            }),
-            treeSetting: {
-                view: {
-                    showLine: false,
-                    selectedMulti: false,
-                    selectedId: '01'
-                },
-                callback: {
-                    onClick: function (e, id, node) {
-                        console.log('tree-checked',node);
-
-                    }
-                }
-            },
-            handelHookRender: function (obj) {
-                var rowId = obj.row.value['$_#_@_id'];
-                var delfun = "data-bind=click:event.del.bind($data," + rowId + ")";
-                var editfun = "data-bind=click:event.edit.bind($data," + rowId + ")";
-                obj.element.innerHTML = '<div><i style="cursor: pointer;" class="op uf uf-pencil font-size-18" title="修改"' + editfun + '></i>' + '<i style="cursor: pointer;" class="font-size-18 op icon uf uf-del title="删除" ' + delfun + '></i></div>';
-                ko.applyBindings(viewModel, obj.element);
-            },
-            initPagniation: function () {
-                var el = document.querySelector("#pagination1");
-                viewModel.pagination1 = new u.pagination({
-                    el: el,
-                    showState: true,
-                    jumppage: true
-                });
-                viewModel.pagination1.on('pageChange', function (currentPage) {
-                    viewModel.currentPage = currentPage + 1;
-
-                });
-                viewModel.pagination1.on('sizeChange', function (pageSize) {
-                    viewModel.pageSize = pageSize;
-                    viewModel.currentPage = 1;
-                });
-            },
-            initPolicyProductPageination: function () {
-                var el = document.querySelector("#policyProductPageination");
-                viewModel.policyProductPageination = new u.pagination({
-                    el: el,
-                    showState: true,
-                    jumppage: true
-                });
-                viewModel.policyProductPageination.on('pageChange', function (currentPage) {
-                    viewModel.policyProductPage.currentPage = currentPage + 1;
-
-                });
-                viewModel.policyProductPageination.on('sizeChange', function (pageSize) {
-                    viewModel.policyProductPage.pageSize = pageSize;
-                    viewModel.policyProductPage.currentPage = 1;
-                });
-            }
-        };
+                ;
         $(element).html(html);
         viewModel.event.pageInit();
 
---
+++ pages/purchaseOrderDetail/wuliu.js
@@ -0,0 +1,99 @@
+var wuliu = [
+    {
+        title: '羊马',
+        progress:50,
+        fields: [
+            {
+                values: [{
+                    column1: "aaaa",
+                    column2: "12",
+                    column3: "13",
+                    id: '0',
+                    pid: ''
+                }, {
+                    column1: "aaaaa",
+                    column2: "22",
+                    column3: "23",
+                    id: '1',
+                    pid: '0'
+                }, {
+                    column1: "31",
+                    column2: "32",
+                    column3: "33",
+                    id: '3',
+                    pid: '1'
+                }
+                ]
+            }, {
+                values: [{
+                    column1: "bbb",
+                    column2: "12",
+                    column3: "13",
+                    id: '0',
+                    pid: ''
+                }, {
+                    column1: "bbb",
+                    column2: "22",
+                    column3: "23",
+                    id: '1',
+                    pid: '0'
+                }, {
+                    column1: "31",
+                    column2: "32",
+                    column3: "33",
+                    id: '3',
+                    pid: '1'
+                }
+                ]
+            }
+        ]
+    }, {
+        title: '清风',
+        progress:10,
+        fields: [
+            {
+                values: [{
+                    column1: "ccc",
+                    column2: "12",
+                    column3: "13",
+                    id: '0',
+                    pid: ''
+                }, {
+                    column1: "ccc",
+                    column2: "22",
+                    column3: "23",
+                    id: '1',
+                    pid: '0'
+                }, {
+                    column1: "31",
+                    column2: "32",
+                    column3: "33",
+                    id: '3',
+                    pid: '1'
+                }
+                ]
+            }, {
+                values: [{
+                    column1: "ddd",
+                    column2: "12",
+                    column3: "13",
+                    id: '0',
+                    pid: ''
+                }, {
+                    column1: "ddd",
+                    column2: "22",
+                    column3: "23",
+                    id: '1',
+                    pid: '0'
+                }, {
+                    column1: "31",
+                    column2: "32",
+                    column3: "33",
+                    id: '3',
+                    pid: '1'
+                }
+                ]
+            }
+        ]
+    }
+];
